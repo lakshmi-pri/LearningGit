2c6809ccf227395223602b99e2ccdb9cd8dc8078
lakshmi-pri <63235258+lakshmi-pri@users.noreply.github.com>
2020-04-23 11:12:17 +0530

    same line na than error varuma

--- testingFile.py
+++ testingFile.py
@@ -4,7 +4,7 @@ greating()
 print("hello")
 
 print("i am now learning git ")
-print("conflit file merge")
+print("conflit file merge same line na than error varuma")
 
 
 Print("now i am merge two branch")
