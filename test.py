988c6e9b6f94f3854cf13d062a83e491309aa537
priya <my@email.com>
2020-04-23 12:45:26 +0530

    wrote a array mul function

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("sjfksddkfsdd")
+import numpy as np
+
+def arrayCreate(r,c):
+	a = np.random.random(([r,c]))
+	return a
+
+a = arrayCreate(2,2)
+print("Array of a : ",a)
+b = arrayCreate(2,2)
+print("Array of b : ",b)
+
+c = np.multiply(a,b)
+print("multiplication of a and b")
+print("Array of c : ",c)
